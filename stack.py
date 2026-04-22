9ed7287edb68d3305622c60ec65a3a10d77a467e
manisandy2 <mani.sandy2@gmail.com>
2026-04-22 19:23:59 +0530

    stack

---
+++ stack.py
@@ -0,0 +1,20 @@
+stack = []
+
+# push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print(stack)
+
+# pop
+stack.pop()
+stack.pop()
+stack.pop()
+
+print(stack)
+
+# peek
+# print(stack[-1])
+
+print(len(stack) == 0)  # Check if stack is empty
